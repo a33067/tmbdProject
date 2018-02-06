8f89b3a9a13f96b8c88a8642dd6c96164607e6d2
joana <jcnasc.90@gmail.com>
2018-02-06 22:44:14 +0000

    no message

--- Dal/CouchDb/couchdb.js
+++ Dal/CouchDb/couchdb.js
@@ -35,7 +35,7 @@ CouchDB.authenticate =function (username, passwd, cb) {
     const path = dbHost+ dbUsers + '/' + username;
     console.log('couchDb: '+path);
     request(path, (err, res, body) => {
-        console.log(err + 'user: '+user);
+        console.log(err + 'user: '+body);
         if(err) return cb(err)
         if(res.statusCode != 200) return cb(null, null, `User ${username} does not exists`)
         console.log(err + 'user: '+user);
