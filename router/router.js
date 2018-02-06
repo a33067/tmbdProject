69731f06a52389c8eea9f64c4b4b9dad89112772
joana <jcnasc.90@gmail.com>
2018-02-06 20:31:46 +0000

    no message

--- router/router.js
+++ router/router.js
@@ -81,7 +81,7 @@ module.exports = function(debug,router,url,movieController,searchController,acto
         // set locals, only providing error in development
         res.locals.message = err.message
         res.locals.error = req.app.get('env') === 'development' ? err : {}
-        debug('router-debug', 'inside erro')
+        console.log('inside erro');
         // render the error page
         res.status(err.status || 500)
         res.render('error')
